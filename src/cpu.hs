0da40ab0888d0b4e63b4838276aa00fc12c8a1be
RossMeikleham <rossmeikleham@hotmail.co.uk>
2014-11-16 02:29:37 +0000

    Added load register from memory instructions

--- src/cpu.hs
+++ src/cpu.hs
@@ -1,6 +1,7 @@
 -- Atari 2600 6507 CPU --
 
 import Data.Word
+import Data.Bits
 import Control.Monad.State.Lazy
 import qualified Data.Vector.Unboxed as VU
 
@@ -80,34 +81,54 @@ getMem loc = do
     (Memory mem) <- getAllMem
     return $ mem VU.! (fromIntegral loc)
 
+
 setMem :: Word8 -> Word16 -> CPUState ()
 setMem val loc = do 
     (Memory mem) <- getAllMem 
     setAllMem $ Memory $ VU.update mem updatedIndex
  where updatedIndex = VU.fromList [(fromIntegral loc, val)]
 
+-- Concatonate 2 bytes into 16 byte little endian word
+-- first byte given is lower half of the word, second is upper half
+concatBytesLe :: Word8 -> Word8 -> Word16
+concatBytesLe b1 b2 = (fromIntegral b1) .|. (shiftL (fromIntegral b2) 8) 
 
-tay = 0xA8
-tax = 0xAA
-tsx = 0xBA
-tya = 0x98
-txa = 0x8A
-txs = 0x9A
-ldann = 0xA9
-ldxnn = 0xA2
-ldynn = 0xA0
-
-executeOpCode :: Word8 -> Word8 -> CPUState ()
-executeOpCode op dat
+executeOpCode :: Word8 -> Word8 -> Word8 -> CPUState ()
+executeOpCode op b2 b3
     -- Register/Immediate to Register Transfer 
-    | op == tay = setY =<< getA
-    | op == tax = setX =<< getA
-    | op == tsx = setX =<< getS
-    | op == tya = setA =<< getY
-    | op == txa = setA =<< getX
-    | op == txs = setS =<< getX
-    | op == ldann = setA dat
-    | op == ldxnn = setX dat
-    | op == ldynn = setY dat
+    | op == 0xA8 = setY =<< getA
+    | op == 0xAA = setX =<< getA
+    | op == 0xBA = setX =<< getS
+    | op == 0x98 = setA =<< getY
+    | op == 0x8A = setA =<< getX
+    | op == 0x9A = setS =<< getX
+    | op == 0xA9 = setA b2
+    | op == 0xA2 = setX b2
+    | op == 0xA0 = setY b2
+
+    -- Load Register From Memory
+    | op == 0xA5 = setA =<< getMem b2'
+    | op == 0xB5 = setA =<< (\x -> getMem $ b2' + (fromIntegral x)) =<< getX -- A = [X + nn]
+    | op == 0xAD = setA =<< getMem word -- A = [nnnn]
+    | op == 0xBD = setA =<< (\x -> getMem $ word + (fromIntegral x)) =<< getX 
+    | op == 0xB9 = setA =<< (\y -> getMem $ word + (fromIntegral y)) =<< getY   
+    | op == 0xA1 = setA =<< getMem . fromIntegral =<<  -- A = [[X + nn]]
+                            (\x -> getMem $ b2' + (fromIntegral x)) =<< getX
+    | op == 0xB1 = setA =<< getMem . fromIntegral =<<  -- A = [[X + nn]]
+                            (\y -> getMem $ b2' + (fromIntegral y)) =<< getY 
+    | op == 0xA6 = setX =<< getMem b2'
+    | op == 0xB6 = setX =<< (\x -> getMem $ b2' + (fromIntegral x)) =<< getY -- A = [X + nn]
+    | op == 0xAE = setX =<< getMem word -- A = [nnnn]
+    | op == 0xBE = setX =<< (\x -> getMem $ word + (fromIntegral x)) =<< getY
+    | op == 0xA4 = setY =<< getMem b2'
+    | op == 0xB4 = setY =<< (\x -> getMem $ b2' + (fromIntegral x)) =<< getX -- A = [X + nn]
+    | op == 0xAC = setY =<< getMem word -- A = [nnnn]
+    | op == 0xBC = setY =<< (\x -> getMem $ word + (fromIntegral x)) =<< getX 
+
+ where b2'  = fromIntegral b2
+       b3'  = fromIntegral b3
+       word = concatBytesLe b2 b3 
+       
+        
 
 
